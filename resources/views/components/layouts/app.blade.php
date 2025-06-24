178055823a5eb25e31a8773be23ae9f7b4e13960
Andres Crucitti <dasc495@gmail.com>
2025-06-24 14:48:00 -0700

    remove debugging code

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,6 @@
   function playAudio(text) {
       const utterance = new SpeechSynthesisUtterance(text);
       const voices = window.speechSynthesis.getVoices();
-      console.log(voices);
       utterance.voice = voices.find(v => v.lang === 'ja-JP' && v.name.includes('Kyoko'));
       speechSynthesis.speak(utterance);
   }
