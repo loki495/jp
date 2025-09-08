45c54a2981bb0f08019b65b30bdcbbcbc35e9882
Andres Crucitti <dasc495@gmail.com>
2025-09-08 16:34:36 -0700

    allow custom speech rate

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,10 +5,11 @@
     </flux:main>
 
 <script>
-  function playAudio(text) {
+  function playAudio(text, rate = 1) {
       const utterance = new SpeechSynthesisUtterance(text);
       const voices = window.speechSynthesis.getVoices();
       utterance.voice = voices.find(v => v.lang === 'ja-JP' && v.name.includes('Kyoko'));
+      utterance.rate = rate;
       speechSynthesis.speak(utterance);
   }
 </script>
